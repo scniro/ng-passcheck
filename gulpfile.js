3f8ff2a9e2e1f5c161655b3ff59ec0af9249d3cf
Sal Niro <salvatore.niro@outlook.com>
2015-10-18 13:37:06 -0600

    sass gulp setu

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+﻿/// <binding ProjectOpened='sass:watch' />
+var gulp = require('gulp');
+
+var uglify = require('gulp-uglify');
+
+var sass = require('gulp-sass');
+
+var autoprefixer = require('gulp-autoprefixer');
+
+var rename = require('gulp-rename');
+
+gulp.task('sass', function () {
+	return gulp.src('demo-site/sass/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('demo-site/css'));
+});
+
+gulp.task('sass:watch', function () {
+	gulp.watch('demo-site/sass/*.scss', ['sass']);
+});
