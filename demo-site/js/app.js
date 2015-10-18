7b4cfe477f65c6c2d823c1cd500db217f1dcf3f0
Sal Niro <salvatore.niro@outlook.com>
2015-10-18 13:21:25 -0600

    adding module

--- demo-site/js/app.js
+++ demo-site/js/app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('app', []);
+﻿var app = angular.module('app', ['ngPasscheck']);
 
 app.controller('ctrl', ['$scope', function($scope) {
 	$scope.init = 'hello init';
--- ng-passcheck/src/ng-passcheck.js
+++ ng-passcheck/src/ng-passcheck.js
@@ -1,1 +1,35 @@
-﻿console.log('ngPasscheck');
+﻿angular.module('ngPasscheck', []).directive('passCheck', function ($compile) {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		scope: {
+			password: '=ngModel'
+		},
+		link: function(scope, elem, attrs) {
+
+			elem.after(angular.element($compile('<span ng-class="{\'weak\' : strength === 0, \'medium\' : strength === 1, \'strong\' : strength === 2}">{{ description }}</span>')(scope)));
+
+			var strong = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{9,})');
+
+			var medium = new RegExp('^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})');
+
+			scope.$watch('password', function(n, o) {
+				if (n && n !== o) {
+					if (strong.test(scope.password)) {
+						scope.strength = 2;
+						scope.description = 'strong';
+					} else if (medium.test(scope.password)) {
+						scope.strength = 1;
+						scope.description = 'ehh';
+					} else {
+						scope.strength = 0;
+						scope.description = 'weak sauce';
+					}
+				} else {
+					scope.strength = null;
+					scope.description = null;
+				}
+			});
+		}
+	}
+});
