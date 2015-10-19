2d21b0e8ccaf8af1c06d916c2b467e208ed4bf50
Sal Niro <salvatore.niro@outlook.com>
2015-10-19 14:31:05 -0600

    configuration provider

--- demo-site/js/app.js
+++ demo-site/js/app.js
@@ -1,5 +1,14 @@
 ﻿var app = angular.module('app', ['ngPasscheck']);
 
+app.config(function (passCheckProvider) {
+	passCheckProvider.init({
+		testCommon: {
+			path: '/ng-passcheck/src/passwords.json',
+			limit: 100
+		}
+	});
+});
+
 app.controller('ctrl', ['$scope', function($scope) {
 	$scope.init = 'hello init';
 }]);
---
+++ ng-passcheck/src/ng-passcheck.js
@@ -0,0 +1,64 @@
+﻿angular.module('ngPasscheck', []).directive('passCheck', function ($compile, passCheckService) {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		scope: {
+			password: '=ngModel'
+		},
+		link: function (scope, elem, attrs) {
+
+			elem.after(angular.element($compile('<span ng-class="{\'weak\' : result.strength === 0, \'medium\' : result.strength === 1, \'strong\' : result.strength === 2}">{{ result.description }}</span>')(scope)));
+
+			scope.$watch('password', function (n, o) {
+				scope.result = n ? passCheckService.analyze(n) : null;
+			});
+		}
+	}
+}).provider('passCheck', function () {
+	return {
+		init: function (options) {
+
+			this.regex = {
+				'strong': options.regex ? options.regex.strong : new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{9,})'),
+				'medium': options.regex ? options.regex.medium : new RegExp('^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})')
+			}
+
+			this.testCommon = options.testCommon ? { 'path': options.testCommon.path, 'limit': options.testCommon.limit } : false;
+		},
+		$get: function () {
+			return {
+				regex: this.regex,
+				testCommon: this.testCommon
+			}
+		}
+	}
+})
+.factory('passCheckService', function (passCheck, $http) {
+
+	if (passCheck.testCommon) {
+		$http.get(passCheck.testCommon.path).then(function (response) {
+			console.log(response);
+		});
+	}
+
+	function analyze(password) {
+		var result = {};
+
+		if (passCheck.regex.strong.test(password)) {
+			result.strength = 2;
+			result.description = 'strong';
+		} else if (passCheck.regex.medium.test(password)) {
+			result.strength = 1;
+			result.description = 'ehh';
+		} else {
+			result.strength = 0;
+			result.description = 'weak sauce';
+		}
+
+		return result;
+	}
+
+	return {
+		'analyze': analyze
+	}
+})
