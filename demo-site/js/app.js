d6992a926cf29a36d0c8d791a033b976aec81b38
Sal Niro <salvatore.niro@outlook.com>
2015-11-04 15:11:03 -0700

    build/cleanup

--- demo-site/js/app.js
+++ demo-site/js/app.js
@@ -1,67 +1,67 @@
 ﻿var app = angular.module('app', ['ngPasscheck']);
 
 app.config(function (passCheckProvider) {
-	passCheckProvider.init({
-		testCommon: {
-			path: '/ng-passcheck/dist/passwords-hashed.json',
-			limit: 10000
-		}
-	});
+    passCheckProvider.init({
+        testCommon: {
+            path: '/ng-passcheck/dist/passwords-hashed.json',
+            limit: 10000
+        }
+    });
 });
 
 app.controller('ctrl', ['$scope', function ($scope) {
 
-	$scope.tabs = [
-		{ 'title': 'Markup', 'url': 'demo-site/template/markup.html' },
-		{ 'title': 'CSS', 'url': 'demo-site/template/css.html' },
-		{ 'title': 'Controller', 'url': 'demo-site/template/controller.html' },
-		{ 'title': 'Config', 'url': 'demo-site/template/config.html' }
-	];
+    $scope.tabs = [
+        { 'title': 'Markup', 'url': 'demo-site/template/markup.html' },
+        { 'title': 'CSS', 'url': 'demo-site/template/css.html' },
+        { 'title': 'Controller', 'url': 'demo-site/template/controller.html' },
+        { 'title': 'Config', 'url': 'demo-site/template/config.html' }
+    ];
 
-	var jsonresult = angular.element(document.getElementById('jsonresult'));
+    var jsonresult = angular.element(document.getElementById('jsonresult'));
 
-	$scope.$on('myPwd:result', function (event, value) {
+    $scope.$on('myPwd:result', function (event, value) {
 
-		jsonresult.html(JSON.stringify(value).replace(/"/g, "'"));
+        jsonresult.html(JSON.stringify(value).replace(/"/g, "'"));
 
-		Prism.highlightElement(jsonresult[0]);
+        Prism.highlightElement(jsonresult[0]);
 
-		$scope.result = value;
-	});
+        $scope.result = value;
+    });
 }]);
 
-app.directive('tabs', ['$timeout', function ($timeout) {
-	return {
-		restrict: 'E',
-		templateUrl: 'demo-site/template/tabs.html',
-		scope: {
-			tabs: '=',
-			selected: '@'
-		},
-		link: function (scope, elem, attrs) {
+app.directive('tabs', [function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'demo-site/template/tabs.html',
+        scope: {
+            tabs: '=',
+            selected: '@'
+        },
+        link: function (scope, elem, attrs) {
 
-			if (scope.tabs) {
-				scope.currentTab = scope.tabs[scope.selected].url;
+            if (scope.tabs) {
+                scope.currentTab = scope.tabs[scope.selected].url;
 
-				scope.onClickTab = function (tab) {
-					scope.currentTab = tab.url;
-				}
+                scope.onClickTab = function (tab) {
+                    scope.currentTab = tab.url;
+                }
 
-				scope.isActiveTab = function (tabUrl) {
-					return tabUrl === scope.currentTab;
-				}
-			}
-		}
-	}
+                scope.isActiveTab = function (tabUrl) {
+                    return tabUrl === scope.currentTab;
+                }
+            }
+        }
+    }
 }]);
 
 app.directive('prism', [function () {
-	return {
-		restrict: 'A',
-		link: function (scope, elem, attrs) {
-			elem.ready(function () {
-				Prism.highlightElement(elem[0]);
-			});
-		}
-	}
+    return {
+        restrict: 'A',
+        link: function (scope, elem, attrs) {
+            elem.ready(function () {
+                Prism.highlightElement(elem[0]);
+            });
+        }
+    }
 }]);
--- gulpfile.js
+++ gulpfile.js
@@ -13,57 +13,57 @@ var crc32 = require('buffer-crc32');
 
 var fs = require('fs');
 
-gulp.task('minify-js', function () {
-	return gulp.src('ng-passcheck/src/*.js')
-		.pipe(uglify())
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest('ng-passcheck/dist'));
+gulp.task('minify-js', function() {
+    return gulp.src('ng-passcheck/src/*.js')
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('ng-passcheck/dist'));
 });
 
-gulp.task('sass', function () {
-	return gulp.src('demo-site/sass/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('demo-site/css'));
+gulp.task('sass', function() {
+    return gulp.src('demo-site/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('demo-site/css'));
 });
 
-gulp.task('sass:watch', function () {
-	gulp.watch('demo-site/sass/*.scss', ['sass']);
+gulp.task('sass:watch', function() {
+    gulp.watch('demo-site/sass/*.scss', ['sass']);
 });
 
-gulp.task('passwords', function () {
-	function isNotEmpty(line) {
-		return Boolean(line.length);
-	}
+gulp.task('passwords', function() {
+    function isNotEmpty(line) {
+        return Boolean(line.length);
+    }
 
-	function parseLine(line) {
-		return line.trim();
-	}
+    function parseLine(line) {
+        return line.trim();
+    }
 
-	var lines = fs.readFileSync('ng-passcheck/src/passwords.txt').toString().split('\n');
-	var result = lines.filter(isNotEmpty).map(parseLine);
-	var json = JSON.stringify({ 'format': 'text', 'dictionary': result });
-	fs.writeFileSync('ng-passcheck/dist/passwords.json', json);
+    var lines = fs.readFileSync('ng-passcheck/src/passwords.txt').toString().split('\n');
+    var result = lines.filter(isNotEmpty).map(parseLine);
+    var json = JSON.stringify({ 'format': 'text', 'dictionary': result });
+    fs.writeFileSync('ng-passcheck/dist/passwords.json', json);
 });
 
 gulp.task('passwords:hashed', function() {
-	function isNotEmpty(line) {
-		return Boolean(line.length);
-	}
+    function isNotEmpty(line) {
+        return Boolean(line.length);
+    }
 
-	function parseLine(line) {
-		return crc32.unsigned(line.trim());
-	}
+    function parseLine(line) {
+        return crc32.unsigned(line.trim());
+    }
 
-	var lines = fs.readFileSync('ng-passcheck/src/passwords.txt').toString().split('\n');
-	var result = lines.filter(isNotEmpty).map(parseLine);
-	var json = JSON.stringify({ 'format': 'crc32', 'dictionary': result });
-	fs.writeFileSync('ng-passcheck/dist/passwords-hashed.json', json);
+    var lines = fs.readFileSync('ng-passcheck/src/passwords.txt').toString().split('\n');
+    var result = lines.filter(isNotEmpty).map(parseLine);
+    var json = JSON.stringify({ 'format': 'crc32', 'dictionary': result });
+    fs.writeFileSync('ng-passcheck/dist/passwords-hashed.json', json);
 });
 
 gulp.task('build', ['minify-js', 'passwords', 'passwords:hashed']);
